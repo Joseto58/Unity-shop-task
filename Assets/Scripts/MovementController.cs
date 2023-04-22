180d278dd697fe6f91d77e4457afa7a66bab77c0
Joseto58 <josetoguzmanr@gmail.com>
2023-04-22 19:26:14 -0400

    Added basic movement

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -1,11 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class ChracterController : MonoBehaviour
+
+public class MovementController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+  PlayerControls _playerControls;
+  BoxCollider2D _collider;
+  Rigidbody2D _rigidbody;
+
+  float _walkingSpeed = 3f;
+
+  Vector2 _currentMovementInput;
+
+  private void Awake() {
+    _playerControls = new PlayerControls();
+
+    //_collider = GetComponent<BoxCollider2D>();
+    _rigidbody = GetComponent<Rigidbody2D>();
+    _playerControls.Movement.move.started += OnMove;
+    _playerControls.Movement.move.performed += OnMove;
+    _playerControls.Movement.move.canceled += OnMove;
+
+    _playerControls.Movement.move.started += OnMove;
+    _playerControls.Movement.interact.started += OnInteract;
+  }
+
+  // Start is called before the first frame update
+  void Start()
     {
         
     }
@@ -15,4 +39,22 @@ public class ChracterController : MonoBehaviour
     {
         
     }
+
+  private void OnMove(InputAction.CallbackContext context) {
+    _currentMovementInput = context.ReadValue<Vector2>();
+    _rigidbody.velocity = _currentMovementInput*_walkingSpeed;
+  }
+
+  private void OnInteract(InputAction.CallbackContext context) {
+    print("Interacting");
+  }
+
+
+  private void OnEnable() {
+    _playerControls.Movement.Enable();
+  }
+
+  private void OnDisable() {
+    _playerControls.Movement.Disable();
+  }
 }
